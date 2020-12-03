246fce39447140bb307a5ecb5c1b04b0218eeb80
vemunderoe <vemunderoe@gmail.com>
2020-12-03 10:44:12 +0100

    Cleaned code and matched requests to backend

--- app.js
+++ app.js
@@ -5,23 +5,45 @@ var clothes = 0;
 var gifts = 0;
 var travel = 0;
 var misc = 0;
+
 const garbageSvg = '<?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="108.294px" height="122.88px" viewBox="0 0 108.294 122.88" enable-background="new 0 0 108.294 122.88" xml:space="preserve"><g><path fill="" d="M4.873,9.058h33.35V6.2V6.187c0-0.095,0.002-0.186,0.014-0.279c0.075-1.592,0.762-3.037,1.816-4.086l-0.007-0.007 c1.104-1.104,2.637-1.79,4.325-1.806l0.023,0.002V0h0.031h19.884h0.016c0.106,0,0.207,0.009,0.309,0.022 c1.583,0.084,3.019,0.76,4.064,1.81c1.102,1.104,1.786,2.635,1.803,4.315l-0.003,0.021h0.014V6.2v2.857h32.909h0.017 c0.138,0,0.268,0.014,0.401,0.034c1.182,0.106,2.254,0.625,3.034,1.41l0.004,0.007l0.005-0.007 c0.851,0.857,1.386,2.048,1.401,3.368l-0.002,0.032h0.014v0.032v10.829c0,1.472-1.195,2.665-2.667,2.665h-0.07H2.667 C1.195,27.426,0,26.233,0,24.762v-0.063V13.933v-0.014c0-0.106,0.004-0.211,0.018-0.315v-0.021 c0.089-1.207,0.624-2.304,1.422-3.098l-0.007-0.002C2.295,9.622,3.49,9.087,4.81,9.069l0.032,0.002V9.058H4.873L4.873,9.058z M77.79,49.097h-5.945v56.093h5.945V49.097L77.79,49.097z M58.46,49.097h-5.948v56.093h5.948V49.097L58.46,49.097z M39.13,49.097 h-5.946v56.093h5.946V49.097L39.13,49.097z M10.837,31.569h87.385l0.279,0.018l0.127,0.007l0.134,0.011h0.009l0.163,0.023 c1.363,0.163,2.638,0.789,3.572,1.708c1.04,1.025,1.705,2.415,1.705,3.964c0,0.098-0.009,0.193-0.019,0.286l-0.002,0.068 l-0.014,0.154l-7.393,79.335l-0.007,0.043h0.007l-0.016,0.139l-0.051,0.283l-0.002,0.005l-0.002,0.018 c-0.055,0.331-0.12,0.646-0.209,0.928l-0.007,0.022l-0.002,0.005l-0.009,0.018l-0.023,0.062l-0.004,0.021 c-0.118,0.354-0.264,0.698-0.432,1.009c-1.009,1.88-2.879,3.187-5.204,3.187H18.13l-0.247-0.014v0.003l-0.011-0.003l-0.032-0.004 c-0.46-0.023-0.889-0.091-1.288-0.202c-0.415-0.116-0.818-0.286-1.197-0.495l-0.009-0.002l-0.002,0.002 c-1.785-0.977-2.975-2.882-3.17-5.022L4.88,37.79l-0.011-0.125l-0.011-0.247l-0.004-0.116H4.849c0-1.553,0.664-2.946,1.707-3.971 c0.976-0.955,2.32-1.599,3.756-1.726l0.122-0.004v-0.007l0.3-0.013l0.104,0.002V31.569L10.837,31.569z M98.223,36.903H10.837 v-0.007l-0.116,0.004c-0.163,0.022-0.322,0.106-0.438,0.222c-0.063,0.063-0.104,0.132-0.104,0.179h-0.007l0.007,0.118l7.282,79.244 h-0.002l0.002,0.012c0.032,0.376,0.202,0.691,0.447,0.825l-0.002,0.004l0.084,0.032l0.063,0.012h0.077h72.695 c0.207,0,0.399-0.157,0.518-0.377l0.084-0.197l0.054-0.216l0.014-0.138h0.005l7.384-79.21L98.881,37.3 c0-0.045-0.041-0.111-0.103-0.172c-0.12-0.118-0.286-0.202-0.451-0.227L98.223,36.903L98.223,36.903z M98.334,36.901h-0.016H98.334 L98.334,36.901z M98.883,37.413v-0.004V37.413L98.883,37.413z M104.18,37.79l-0.002,0.018L104.18,37.79L104.18,37.79z M40.887,14.389H5.332v7.706h97.63v-7.706H67.907h-0.063c-1.472,0-2.664-1.192-2.664-2.664V6.2V6.168h0.007 c-0.007-0.22-0.106-0.433-0.259-0.585c-0.137-0.141-0.324-0.229-0.521-0.252h-0.082h-0.016H44.425h-0.031V5.325 c-0.213,0.007-0.422,0.104-0.576,0.259l-0.004-0.004l-0.007,0.004c-0.131,0.134-0.231,0.313-0.259,0.501l0.007,0.102V6.2v5.524 C43.554,13.196,42.359,14.389,40.887,14.389L40.887,14.389z"/></g></svg>';
 const editSvg = '<?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 122.88 121.96" style="enable-background:new 0 0 122.88 121.96" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path class="st0" d="M107.73,1.31c-0.96-0.89-2.06-1.37-3.29-1.3c-1.23,0-2.33,0.48-3.22,1.44l-7.27,7.54l20.36,19.67l7.33-7.68 c0.89-0.89,1.23-2.06,1.23-3.29c0-1.23-0.48-2.4-1.37-3.22L107.73,1.31L107.73,1.31L107.73,1.31z M8.35,5.09h50.2v13.04H14.58 c-0.42,0-0.81,0.18-1.09,0.46c-0.28,0.28-0.46,0.67-0.46,1.09v87.71c0,0.42,0.18,0.81,0.46,1.09c0.28,0.28,0.67,0.46,1.09,0.46 h87.71c0.42,0,0.81-0.18,1.09-0.46c0.28-0.28,0.46-0.67,0.46-1.09V65.1h13.04v48.51c0,2.31-0.95,4.38-2.46,5.89 c-1.51,1.51-3.61,2.46-5.89,2.46H8.35c-2.32,0-4.38-0.95-5.89-2.46C0.95,118,0,115.89,0,113.61V13.44c0-2.32,0.95-4.38,2.46-5.89 C3.96,6.04,6.07,5.09,8.35,5.09L8.35,5.09z M69.62,75.07c-2.67,0.89-5.42,1.71-8.09,2.61c-2.67,0.89-5.35,1.78-8.09,2.67 c-6.38,2.06-9.87,3.22-10.63,3.43c-0.75,0.21-0.27-2.74,1.3-8.91l5.07-19.4l0.42-0.43l20.02,20.02L69.62,75.07L69.62,75.07 L69.62,75.07z M57.01,47.34L88.44,14.7l20.36,19.6L77.02,67.35L57.01,47.34L57.01,47.34z"/></g></svg>'
 
 
+const API_URL = 'https://evening-refuge-60189.herokuapp.com';
 
-const retrieveUser = () => {
+const retrieveExpenses = () => {
+    axios({
+        method: 'get',
+        url: `${API_URL}/expenses`,
+    })
+    .then((result) => {
+        calculateLatestExpenses(result.data);
+        calculateCategories(result.data);
+        calculateDoughnutChart();
+        calculateTableChart();
+    })
+    .catch((error) => {
+        //console.log(error.response);
+        if (error.response.status === 401) {            
+            retrieveTestData();
+            snackbar('Loaded test data. Log in to store your own data', '#F8C630')
+        }
+    })
+}
 
-    const userTest = {
-        user_id: 0,
-    };
-
-    if (!localStorage.getItem('user')) {
-        snackbar('You should to login to store your own data!', '#FFCC00');
-        return userTest;
-    }
-
-    return JSON.parse(localStorage.getItem('user'));
+const retrieveTestData = () => {
+    axios({
+        method: 'get',
+        url: `${API_URL}/expensestd`,
+    })
+    .then((result) => {
+        calculateLatestExpenses(result.data);
+        calculateCategories(result.data);
+    })
+    .catch((error) => {
+        console.log(error.response);        
+    })
 }
 
 const register = () => {
@@ -31,7 +53,7 @@ const register = () => {
 
     axios({
             method: 'post',
-            url: 'https://evening-refuge-60189.herokuapp.com/users/register',
+            url: `${API_URL}/users/register`,
             data: {
                 name: name.value,
                 email: email.value,
@@ -56,7 +78,7 @@ const login = () => {
 
     axios({
             method: 'post',
-            url: 'https://evening-refuge-60189.herokuapp.com/users/login',
+            url: `${API_URL}/users/login`,
             withCredentials: 'true',
             data: {
                 email: email.value,
@@ -64,12 +86,13 @@ const login = () => {
             }
         })
         .then((result) => {
+            console.log(result);
             if (result.data.message == 'Logged in successfully') {
                 localStorage.setItem('user', JSON.stringify(result.data.user));
                 // trigger snackbar with success
                 snackbar(result.data.message, '#28A745');
                 setTimeout(function () {
-                    window.location.href = 'index.html';
+                    window.location.href = 'index.html'; 
                 }, 3000);
             } else {
                 // trigger snackbar with error message
@@ -87,7 +110,7 @@ const logout = () => {
 
     axios({
             method: 'post',
-            url: 'https://evening-refuge-60189.herokuapp.com/users/logout',
+            url: `${API_URL}/users/logout`,
             withCredentials: 'true',
             data: {
                 email: user.email
@@ -96,7 +119,7 @@ const logout = () => {
         .then((result) => {
             if (result.data.message == 'Logged out successfully') {
                 localStorage.removeItem('user');
-                snackbar(result.data.message, '#28A745')
+                snackbar(result.data.message, '#28A745', 3000)
                 setTimeout(function () {
                     window.location.href = 'login.html';
                 }, 3000);
@@ -110,50 +133,39 @@ const logout = () => {
         });
 }
 
-const calculateCategories = () => {
+const calculateCategories = (data) => {
 
-    //const user = retrieveUser();
+    let dataPulled = data;    
 
-    axios({
-            method: 'get',
-            url: 'https://evening-refuge-60189.herokuapp.com/expensestd'
-        })
-        .then((response) => {
-            let dataPulled = response.data;
+    for (let i = 0; i < dataPulled.length; i++) {
 
-            for (let i = 0; i < dataPulled.length; i++) {
+        switch (dataPulled[i].category) {
+            case "food":
+                food += parseFloat(dataPulled[i].value);
+                break;
+            case "rent":
+                rent += parseFloat(dataPulled[i].value);
+                break;
+            case "utilities":
+                utilities += parseFloat(dataPulled[i].value);
+                break;
+            case "clothes":
+                clothes += parseFloat(dataPulled[i].value);
+                break;
+            case "gifts":
+                gifts += parseFloat(dataPulled[i].value);
+                break;
+            case "travel":
+                travel += parseFloat(dataPulled[i].value);
+                break;
+            case "misc":
+                misc += parseFloat(dataPulled[i].value);
+                break;
+        }
+    }
 
-                switch (dataPulled[i].category) {
-                    case "food":
-                        food += parseFloat(dataPulled[i].value);
-                        break;
-                    case "rent":
-                        rent += parseFloat(dataPulled[i].value);
-                        break;
-                    case "utilities":
-                        utilities += parseFloat(dataPulled[i].value);
-                        break;
-                    case "clothes":
-                        clothes += parseFloat(dataPulled[i].value);
-                        break;
-                    case "gifts":
-                        gifts += parseFloat(dataPulled[i].value);
-                        break;
-                    case "travel":
-                        travel += parseFloat(dataPulled[i].value);
-                        break;
-                    case "misc":
-                        misc += parseFloat(dataPulled[i].value);
-                        break;
-                }
-            }
-            calculateDoughnutChart();
-            calculateTableChart();
-
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    calculateDoughnutChart();
+    calculateTableChart();
 }
 
 
@@ -247,32 +259,20 @@ const calculateTableChart = () => {
     });
 }
 
-const postMethodRegister = () => {
+const calculateLatestExpenses = (data) => {
+    
+    let parentList = '<ul>'
+    var expenseList = data;
+    for (let i = expenseList.length - 1; i >= 0; i--) {
+        if (typeof expenseList[i] == "undefined") {
+            break;
+        }
+        parentList += '<li>' + `(£${expenseList[i].value})` + " " + expenseList[i].name + `<button onclick="removeExpenses(${expenseList[i].id})" class="remove">&#x1F5D1;</button></li>`;
 
-    //const user = retrieveUser();
+    }
 
-    axios({
-            method: 'get',
-            url: `https://evening-refuge-60189.herokuapp.com/expensestd`
-        })
-        .then((response) => {
-            let parentList = '<ul>'
-            var expenseList = response.data
-            for (let i = expenseList.length - 1; i >= 0; i--) {
-                if (typeof expenseList[i] == "undefined") {
-                    break;
-                }
-                parentList += '<li>' + `(£${expenseList[i].value})` + " " + expenseList[i].name + `<div class="editContainer"><button class="edit">${editSvg}</button>` + `<button onclick="removeExpenses(${expenseList[i].id})" class="remove">${garbageSvg}</button></div></li>`;
-
-            }
-
-            parentList += '</ul>';
-            document.getElementById('expenses').innerHTML = parentList;
-            calculateCategories();
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    parentList += '</ul>';
+    document.getElementById('expenses').innerHTML = parentList;
 }
 
 
@@ -282,55 +282,52 @@ const postExpenses = () => {
     var item = document.getElementById("item");
     var type = document.getElementById("category");
 
-    const user = retrieveUser();
-    if (user.user_id === 0) {
-        snackbar('You need to log in if you want to track expenses!', '');
-        return;
-    }
-
     axios({
             method: 'post',
-            url: 'https://evening-refuge-60189.herokuapp.com/expenses/add',
+            url: `${API_URL}/expenses/add`,
             data: {
-                user_id: user.user_id,
                 value: money.value,
                 name: item.value,
                 category: type.value
             }
         })
         .then((response) => {
-            postMethodRegister();
+            //console.log(response);
+            retrieveExpenses();
             snackbar(response.data.message, '#28A745');
         })
         .catch((error) => {
-            console.log(error);
+            //console.log(error.response);         
+            if (error.response.status === 401) {
+                snackbar('You need to log in', '#F8C630');
+            }
         });
 }
 
 
 const removeExpenses = (id) => {
 
-
     axios({
             method: 'post',
-            url: 'https://evening-refuge-60189.herokuapp.com/expenses/delete',
+            url: `${API_URL}/expenses/delete`,
             data: {
                 expense_id: id
             }
         })
         .then((result) => {
             snackbar(result.data.message, '#28A745');
-            postMethodRegister();
+            retrieveExpenses();
             console.log(result);
-
-
         })
         .catch((error) => {
-            console.log(error);
+            //console.log(error.response);
+            if (error.response.status === 401) {
+                snackbar('You need to log in', '#F8C630');
+            }
         })
 }
 
-const snackbar = (message, color) => {
+const snackbar = (message, color, time) => {
     // Get the snackbar DIV
     let x = document.getElementById("snackbar");
     x.innerHTML = message;
@@ -341,5 +338,5 @@ const snackbar = (message, color) => {
     // After 3 seconds, remove the show class from DIV
     setTimeout(function () {
         x.className = x.className.replace("show", "");
-    }, 3000);
+    }, time ? time : 3000);
 }
